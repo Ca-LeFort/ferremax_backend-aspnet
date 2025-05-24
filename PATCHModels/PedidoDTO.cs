9b007b9e4a84bb021bf9b29a08b6050d1aa70764
Ca-LeFort <ca.lefort@duocuc.cl>
2025-05-24 16:09:36 -0400

    Mercado Pago integrado y avance otros controladores

---
+++ PATCHModels/PedidoDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace ApiPrincipal_Ferremas.PATCHModels
+{
+    public class PedidoDTO
+    {
+        public int? PrecioTotal { get; set; }
+
+        public int? IdCarrito { get; set; }
+
+        public string? RutCliente { get; set; }
+
+        public int? IdEstPedido { get; set; }
+
+        public int? IdDespacho { get; set; }
+
+        public int? IdSucursal { get; set; }
+    }
+}
